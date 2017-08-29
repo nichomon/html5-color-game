83b45331dbe37888e73f3447d9dd5ecb559b7f42
nfd <nickfreemandesign>
2017-08-29 10:43:08 -0700

    mid way in db setup

---
+++ db/db-config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var knex = require('knex')({
+  client: 'sqlite3',
+  connection: {
+    filename: path.join(_dirname, './html5-color-game.sqlite')
+  },
+  useNullAsDefault: true
+});
+var db = require('bookshelf')(knex);
+
+
+db.knex.schema.hasTable('highScores').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('highScores', function (link) {
+      link.increments('id').primary();
+      link.string('userName', 255);
+      link.integer('highScore');
+      link.integer('timesPlayed');
+      link.timestamps();
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
+
+module.exports = db;
